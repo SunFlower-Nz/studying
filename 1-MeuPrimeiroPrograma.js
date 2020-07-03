fb3f1de3eec15416b52680a7f1fda64c9d0dae71
Eduardo Tavares <59029161+SunFlower-Nz@users.noreply.github.com>
2020-07-02 21:04:02 -0300

    Create 1-MeuPrimeiroPrograma.js

---
+++ 1-MeuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Me Primeiro Programa: Trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
